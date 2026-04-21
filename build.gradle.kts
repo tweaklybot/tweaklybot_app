0702ec82267005d03ff7cd4e26a1664a524f5657
tweaklybot <tweakly81@gmail.com>
2026-04-21 17:20:13 +0300

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,5 +5,6 @@ plugins {
     alias(libs.plugins.hilt) apply false
     alias(libs.plugins.ksp) apply false
     alias(libs.plugins.kotlin.serialization) apply false
+    alias(libs.plugins.compose.compiler) apply false // Добавлен новый плагин
     id("com.google.gms.google-services") version "4.4.2" apply false
 }
